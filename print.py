7f611fcc41a1444d2b78a912b147ec3f17f2166d
Ryan Mayer <mayer.ryan.0789@gmail.com>
2020-10-27 17:35:44 +0000

    copying local files with python

---
+++ print.py
@@ -0,0 +1,6 @@
+challenge= [1, 2, 3,]
+abc= ["a", "b", "c",]
+dcb= [1, 4, 7, "yeet"]
+challenge.append(abc)
+challenge.append(dcb)
+print(f"I think we can all agree the The Lion King is improved be a good {challenge[4][3]}.")
---
+++ salty.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+icecream= ["flavors", "salty"]
+icecream.append(99)
+name= input("What's your name?")
+print(f"{icecream[2]} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
